a92983b4dbac38744e73214e646ad796c2ab1f07
mdschramm <markdj10025@gmail.com>
2018-06-26 13:54:43 -0400

    Merge files using fluent-ffmpeg

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
-const http = require('http'),
-    server = http.createServer((request,response)=>{
-        response.writeHead(200,{'Content-Type':'text/plain'});
-        response.write('Hello world');
-        response.end();
+const express = require('express'),
+      bodyParser = require('body-parser'),
+      fluentFfmpeg = require('fluent-ffmpeg');
+
+
+// https://stackoverflow.com/questions/5710358/how-to-retrieve-post-query-parameters
+const app = express();
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+//
+
+let handleMerge = (res, links ) => {
+    let mergedVideo = fluentFfmpeg();
+    // https://stackoverflow.com/questions/28877848/merge-multiple-videos-using-node-fluent-ffmpeg
+    links.forEach(name => {
+        mergedVideo.addInput(name);
     });
-server.listen(3000,()=>{
-    console.log('Node server created at port 3000');
+    mergedVideo.mergeToFile('./mergedVideo.mp4', './tmp/');
+    //
+    res.status(200).json({response: 200});
+};
+
+app.get('/', (req, res) => res.send('Hello World!'));
+app.post('/merge', (req, res) => {
+    return handleMerge(res, req.body.links);
 });
+
+
+app.listen(3000, () => console.log('Running vidmerger!'));
