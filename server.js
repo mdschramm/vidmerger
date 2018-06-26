f0bb3496c686e0fa46c8efa0a158249beb25b775
mdschramm <markdj10025@gmail.com>
2018-06-26 14:29:43 -0400

    serve video via html5 video player or /video path

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require('express'),
       bodyParser = require('body-parser'),
-      fluentFfmpeg = require('fluent-ffmpeg');
+      fluentFfmpeg = require('fluent-ffmpeg'),
+      fs = require('fs');
 
 
 // https://stackoverflow.com/questions/5710358/how-to-retrieve-post-query-parameters
 const app = express();
 app.use( bodyParser.json() );
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
 //
 
 let handleMerge = (res, links ) => {
@@ -22,7 +20,48 @@ let handleMerge = (res, links ) => {
     res.status(200).json({response: 200});
 };
 
-app.get('/', (req, res) => res.send('Hello World!'));
+let home = (req, res) => {
+    fs.readFile('index.html', (err, data) => {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    });
+};
+
+app.get('/', home);
+//https://medium.com/@daspinola/video-stream-with-node-js-and-html5-320b3191a6b6
+app.get('/video', (req, res) => {
+    const path = 'mergedVideo.mp4';
+    const stat = fs.statSync(path);
+    const fileSize = stat.size;
+    const range = req.headers.range;
+    if (range) {
+        const parts = range.replace(/bytes=/, "").split("-");
+        const start = parseInt(parts[0], 10);
+        const end = parts[1]
+            ? parseInt(parts[1], 10)
+            : fileSize-1;
+        const chunksize = (end-start)+1;
+        const file = fs.createReadStream(path, {start, end});
+        const head = {
+            'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': chunksize,
+            'Content-Type': 'video/mp4',
+        };
+        res.writeHead(206, head);
+        file.pipe(res);
+    } else {
+        const head = {
+            'Content-Length': fileSize,
+            'Content-Type': 'video/mp4',
+        };
+        res.writeHead(200, head);
+        fs.createReadStream(path).pipe(res)
+    }
+});
+//
+
 app.post('/merge', (req, res) => {
     return handleMerge(res, req.body.links);
 });
